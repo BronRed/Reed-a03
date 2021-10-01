e9ebfd98d941d12fca57e1094b4b7e753aae5d7c
Brian <Brian.reed1202@gmail.com>
2021-10-01 11:19:30 -0400

    added .ignore and pseudocode for ex24

---
+++ exercise24/src/main/java/Solution24.java
@@ -0,0 +1,15 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Brian Reed
+ */
+import java.util.Scanner;
+public class Solution24 {
+    public static void main(String[] args){
+        //output "Enter two strings and I'll tell you if they are anagrams:"
+        //prompt "Enter the first string:"
+        //get first string
+        //prompt "Enter the second string:"
+        //get second string
+        //compare first string and second string and determine if is anagram
+    }
+}
