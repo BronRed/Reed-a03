40679329e5a749b976a7a530dfc9a58a64ecea6c
Brian <Brian.reed1202@gmail.com>
2021-10-01 12:32:21 -0400

    added pseudocode for ex25

---
+++ exercise25/src/main/java/Solution25.java
@@ -0,0 +1,21 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Brian Reed
+ */
+import java.util.Scanner;
+public class Solution25 {
+    public static void main(String[] args){
+        //prompt user for password
+        //pass password to passwordValidator
+        //have some other function validate weak, strong, very strong password
+        //print weak, strong, very strong
+    }
+    public int passwordValidator(){
+        //take input string from main
+        //validatorNum increases by 1 with each passed case
+        //case1: contains numbers
+        //case2: contains letters
+        //case3: at least 8 characters
+        //case4: contains special characters
+    }
+}
