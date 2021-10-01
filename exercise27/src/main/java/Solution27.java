4f1f5f40d22c7057c1b369bf5545bef1c39c698f
Brian <Brian.reed1202@gmail.com>
2021-10-01 14:44:48 -0400

    added pseudocode for ex27

---
+++ exercise27/src/main/java/Solution27.java
@@ -0,0 +1,11 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Brian Reed
+ */
+public class Solution27 {
+    public static void main(String[] args){
+        Employee employee = new Employee();
+        //pass to Employee to get employee data
+        //pass to validation to check if all data is correct
+    }
+}
