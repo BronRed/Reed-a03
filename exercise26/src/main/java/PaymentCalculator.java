91c7e4206592b68983c91acc74037b46b5880d4f
Brian <Brian.reed1202@gmail.com>
2021-10-01 13:39:00 -0400

    added pseudocode for ex26

---
+++ exercise26/src/main/java/PaymentCalculator.java
@@ -0,0 +1,14 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Brian Reed
+ */
+public class PaymentCalculator {
+    //prompt user for balance
+    //prompt user for APR
+    //prompt user for monthly payment
+    //calculate months until paid off
+    public int calculateMonthsUntilPaidOff(){
+        //use formula n = -(1/30) * log(1 + b/p * (1 - (1 + i)^30)) / log(1 + i)
+        //return the number of months
+    }
+}
---
+++ exercise26/src/main/java/Solution26.java
@@ -0,0 +1,11 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Brian Reed
+ */
+import java.util.Scanner;
+public class Solution26 {
+    public static void main(String[] args){
+        //make class PaymentCalculator
+        //print number of months until paid off
+    }
+}
