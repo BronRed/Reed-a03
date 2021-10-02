1666999baf9c2f8b64cffd35eb7997ebfb35dfc4
Brian <Brian.reed1202@gmail.com>
2021-10-02 15:36:10 -0400

    added pseudocode for ex29

---
+++ exercise29/src/main/java/Calculator.java
@@ -0,0 +1,9 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Brian Reed
+ */
+public class Calculator {
+    //getRate
+    //calculateYears
+    //try catch for illegal input EG: denominator 0
+}
---
+++ exercise29/src/main/java/Solution29.java
@@ -0,0 +1,12 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Brian Reed
+ */
+public class Solution29 {
+    public static void main(String[] args){
+        //build object calculator to hold all important data
+        //use methods in calculator class to get important data
+        //use try catch to handle an illegal input EG: denominator 0
+        //pass calculated value to main and print result
+    }
+}
