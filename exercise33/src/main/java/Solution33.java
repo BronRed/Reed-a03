38224bcf1f01174e98742f74efdeddf2039b8ec4
Brian <Brian.reed1202@gmail.com>
2021-10-03 02:21:25 -0400

    added pseudocode for ex33

---
+++ exercise33/src/main/java/Solution33.java
@@ -0,0 +1,10 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Brian Reed
+ */
+public class Solution33 {
+    //pre generated solutions to answer associated with number
+    //prompt user for a question
+    //choose solution based on random number generation.
+    //output random solution
+}
