650f2359359d36c3d60ea28418087b19e41ae3e5
Brian <Brian.reed1202@gmail.com>
2021-10-02 21:59:47 -0400

    aded pseudocode for ex31

---
+++ exercise31/src/main/java/Solution31.java
@@ -0,0 +1,13 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Brian Reed
+ */
+import java.util.Scanner;
+public class Solution31 {
+    public static void main(String[] args) {
+        //prompt user for age and heart rate
+        //send info to calculator class
+        //use TargetHeartRate = (((220 − age) − restingHR) × intensity) + restingHR
+        //return result and put into table
+    }
+}
