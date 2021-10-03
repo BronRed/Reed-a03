3e17e3308e61c4dacd05cdee50325b759b878496
Brian <Brian.reed1202@gmail.com>
2021-10-03 14:31:38 -0400

    added pseudocode fro ex35

---
+++ exercise35/src/main/java/Solution35.java
@@ -0,0 +1,13 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Brian Reed
+ */
+public class Solution35 {
+    public static void main(String[] args){
+        //make arraylist
+        //do{add name}
+        //while(string added is not empty)
+        //remove empty name from list
+        //make random variable and select name from list via variable
+    }
+}
