43b77b12dffef65c859aa238e1e0086d9fed8a8f
Brian <Brian.reed1202@gmail.com>
2021-10-03 00:56:58 -0400

    added pseudocode for ex32

---
+++ exercise32/src/main/java/Solution32.java
@@ -0,0 +1,13 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Brian Reed
+ */
+public class Solution32 {
+    //prompt user for difficulty level
+    //pick random number based on difficulty number
+    //make counting loop
+    //inside loop take input from user and compare to secret number
+    //if secret number is less than number "you guessed too high"
+    //if secret number is higher than number "you guessed too low"
+    //if secret number = number "you guessed the number in X turns!""do you want to play again?"
+}
