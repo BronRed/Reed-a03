2b2d41221aa403db12cf92a2a35350a8a7b7e512
Brian <Brian.reed1202@gmail.com>
2021-10-02 21:20:00 -0400

    added pseudocode for ex30

---
+++ exercise30/src/main/java/Solution30.java
@@ -0,0 +1,10 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Brian Reed
+ */
+public class Solution30 {
+    //create nested loop to multiply 1st number by 2nd number
+    //1*1, 1*2, 1*3, ... , 1*12, 2*1, 2*2, 2*3, ... , 2*12, ...
+    //print each value and separate with \t
+    //print \n after each outer loop
+}
