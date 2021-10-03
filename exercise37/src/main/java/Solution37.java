6ba86bb81834aae3c3edef42af418ea39c4cba4c
Brian <Brian.reed1202@gmail.com>
2021-10-03 16:48:33 -0400

    added pseudocode for ex37

---
+++ exercise37/src/main/java/Solution37.java
@@ -0,0 +1,16 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Brian Reed
+ */
+public class Solution37 {
+    public static void main(String[] args){
+        //pre-generated arraylist for letters, numbers, special characters
+        //prompt user for length of password
+        //prompt user for number of special characters
+        //prompt user for number of numbers
+        //get random special characters equal to chosen number
+        //get random numbers equal to chosen number
+        //mix characters together in random order
+        //print randomly generated password
+    }
+}
