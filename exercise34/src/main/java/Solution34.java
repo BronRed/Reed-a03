b9a01205b2f77a5c46833bb96b223094fa5d271f
Brian <Brian.reed1202@gmail.com>
2021-10-03 14:00:22 -0400

    added pseudocode for ex34

---
+++ exercise34/src/main/java/Solution34.java
@@ -0,0 +1,11 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Brian Reed
+ */
+public class Solution34 {
+    public static void main(String[] args){
+        //create arraylist of employees{John Smith, Jackie Jackson, Chris Jones, Amanda Cullen, Jeremy Goodwin}
+        //take input of name to delete
+        //remove name from arraylist
+    }
+}
