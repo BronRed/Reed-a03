092c2711343d6c335fb415a940f25b5a4282e56a
Brian <Brian.reed1202@gmail.com>
2021-10-03 17:56:30 -0400

    added pseudocode for ex38

---
+++ exercise38/src/main/java/Solution38.java
@@ -0,0 +1,10 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 first_name last_name
+ */
+public class Solution38 {
+    //prompt user for list of numbers
+    //input numbers in an array
+    //use n%2=0 to determine even numbers
+    //print even numbers
+}
